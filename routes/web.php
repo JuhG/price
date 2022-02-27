8915121c199f061439fb293e200e026a2eafd55b
Gabor Juhasz <gabor.juhasz@accretivetg.com>
2022-02-27 09:19:46 +0100

    feat: fetch price

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\DomCrawler\Crawler;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+function formattedToFloat(string $number): float
+{
+    $number = preg_replace('/[^0-9,-]/', '', $number);
+    $number = str_replace(',', '.', $number);
+    return (float) $number;
+}
+
+function getPrice (string $url, string $selector): float {
+    $html = Http::get($url);
+    $crawler = new Crawler($html);
+    $crawler = $crawler->filter($selector);
+    if ($crawler->count() === 0) {
+        throw new \Exception('Price not found, please adjust selector!');
+    }
+    $number = $crawler->first()->text();
+    return formattedToFloat($number);
+}
+
 Route::get('/', function () {
-    return view('welcome');
+     return getPrice('https://www.moemax.hu/p/modern-living-tkezoasztal-herkules-001630002101', '#currentPrice');
 });
